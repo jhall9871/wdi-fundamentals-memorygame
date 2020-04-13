3aac7a8128491cddafad62cbc7e0cf26266ac7bd
jhall9871 <james@jameshallmusic.com>
2020-04-13 10:13:25 -0500

    created functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,21 @@ console.log("Up and running!")
 
 const cards = ["queen", "jack", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.")
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.")
+    }
   }
 }
+
+function flipCard(cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+flipCard(2);
+flipCard(3);
