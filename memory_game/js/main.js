c4e30522e68636d681e0bcd2755be23e7ddfddb6
jhall9871 <james@jameshallmusic.com>
2020-04-13 10:55:24 -0500

    added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,32 @@
 console.log("Up and running!")
 
-const cards = ["queen", "jack", "king", "king"];
+const cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "kind",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
       console.log("You found a match!");
     } else {
       console.log("Sorry, try again.")
@@ -14,9 +35,14 @@ function checkForMatch() {
 }
 
 function flipCard(cardID) {
-  console.log("User flipped " + cards[cardID]);
+  console.log("User flipped " + cards[cardID].rank);
   cardsInPlay.push(cards[cardID]);
+  console.log(cards[cardID].cardImage);
+  console.log(cards[cardID].suit);
   checkForMatch();
 }
+
+
+
 flipCard(2);
 flipCard(3);
